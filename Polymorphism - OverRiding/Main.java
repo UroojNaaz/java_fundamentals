e991514aa4969489cc0525cbc605d269383be6b8
UROOJ NAZ <142103317+UroojNaaz@users.noreply.github.com>
2024-08-06 17:37:58 +0500

    Polymorphism-OverRiding in OOPs

---
+++ Polymorphism - OverRiding/Main.java
@@ -0,0 +1,29 @@
+public class Animal {
+    public void makeSound() {
+        System.out.println("Animal sound");
+    }
+}
+
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Bark");
+    }
+}
+
+public class Cat extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myDog.makeSound(); // Output: Bark
+        myCat.makeSound(); // Output: Meow
+    }
+}
